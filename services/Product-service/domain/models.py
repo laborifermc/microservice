194eff3a5942ebace6439c4bd4f86da0660a2a18
laborifermc <laborifermc@gmail.com>
2026-02-24 09:31:36 +0100

    Sans prix et sans stock dans le product

--- services/Product-service/domain/models.py
+++ services/Product-service/domain/models.py
@@ -6,19 +6,6 @@ from typing import Optional
 class Product:
     name: str
     description: str
-    price: float
-    stock_quantity: int
-    stock: str  
     category: str
     id: UUID = field(default_factory=uuid4)
     is_active: bool = True
-
-    def update_price(self, new_price: float):
-        if new_price < 0:
-            raise ValueError("Le prix ne peut pas être négatif")
-        self.price = new_price
-
-    def update_stock(self, quantity: int):
-        if quantity < 0:
-            raise ValueError("Le stock ne peut pas être inférieur à zéro")
-        self.stock_quantity = quantity
